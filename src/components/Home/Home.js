ee375ac7afdcf69d8532d342e4666122f70adfd3
Anthony Ramirez <ar4477@nyu.edu>
2020-07-31 23:38:28 -0400

    add homepage layout and add css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import './Home.css'
 
 export default function Home() {
     return (
-        <div>
-            home
-        </div>
+        <>
+            <div className="home-video-container">
+                <video src=""></video>
+            </div>
+
+            <div className="home-container large-container">
+                <div className="latest-single-container">
+                    <h2>Latest Single Out</h2>
+                </div>
+                <div className="home-full-width-container">
+                    <div className="home-full-width-inner-container">
+                        <h2>Latest Visuals</h2>
+                        <h4>See all visuals</h4>
+                    </div>
+                    
+
+                </div>
+                <div className="home-full-width-container">
+                    <div className="home-full-width-inner-container">
+                        <h2>Photos</h2>
+                        <h4>See all photos</h4>
+                    </div>
+                </div>
+                
+                <div className="instagram-feed-container">
+                    
+                    
+                </div>
+            </div>
+        </>
     )
 }
--- src/components/Visuals/Visuals.js
+++ src/components/Visuals/Visuals.js
@@ -6,12 +6,16 @@ export default function Visuals() {
         <div className="large-container">
             <h1>Visuals</h1>
             <div className="visuals-container">
-                <div className="visual-container">
-                    <h2 className="visual-title">Title of Visual</h2>
+                <div className="home-full-width-container">
+                    <div className="home-full-width-inner-container">
+                        <h2>Title of Visual</h2>
+                    </div>
                     <video src=""></video>
                 </div>
-                <div className="visual-container">
-                    <h2 className="visual-title">Title of Visual</h2>
+                <div className="home-full-width-container">
+                    <div className="home-full-width-inner-container">
+                        <h2>Title of Visual</h2>
+                    </div>
                     <video src=""></video>
                 </div>
             </div>
