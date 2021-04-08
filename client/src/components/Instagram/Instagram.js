96232daa3a97108d3c39357bcf81dfb864452570
Anthony Ramirez <ar4477@nyu.edu>
2021-04-08 18:07:15 -0400

    update ig auth token

--- client/src/components/Instagram/Instagram.js
+++ client/src/components/Instagram/Instagram.js
@@ -4,7 +4,7 @@ import Instafeed from './instafeed'
 export default function Instagram() {
     useEffect(() => {
         var feed = new Instafeed({
-            accessToken: 'IGQVJXTkNrM3VidnFXMFFMZAHhmLWRJdUM0OUpUNW1CbjEwTGRhMVE2dVZAPV0FtQnpVSXN5VFpDMFJHTno0azh5bm5iMVJRZA2dCangxTWxGbmk3VkQ3azhYbXE4WnFsTUpnVFY5VDZAxUk1TNkowT3I5bAZDZD',
+            accessToken: 'IGQVJWX2JpSlJERjZAyZAGs2VW1fNEdfZAFFRZAlBtQkFkb19HWTVrN1hkVjhFOVg0bnJPTXdFWnl4MWI5c19pRXhqQ0d5LVJvdnM5OTdnTGQ5WE9iN01la1dVUmNlOVliMF9wdW9oUXg1cUlmaWQxZAUpJbQZDZD',
             limit: 3
         })
         feed.run();
